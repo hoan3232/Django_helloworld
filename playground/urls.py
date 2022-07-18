a53ee2278e82430e7738f19a99935174ab82869b
ThongP <gbrider1403@gmail.com>
2022-07-18 13:43:35 +0700

    Added things and stuffs

--- playground/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-#URLconf
-urlpatterns = [
-    path('HelloWorld/', views.HelloWorld),
-    path('Test/', views.Test)
-]
---
+++ src/playground/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+#URLconf
+urlpatterns = [
+    path('HelloWorld/', views.HelloWorld),
+    path('Test/', views.Test)
+]
