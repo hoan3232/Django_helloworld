367f1c7c918ac2858f66a6bcfdd4e9f82547ff5a
ThongP <gbrider1403@gmail.com>
2022-09-06 14:06:26 +0700

    Forced login, login landing page

--- src/user_auth/urls.py
+++ src/user_auth/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signin_user',views.signin_user, name="signin"),
+    path('signin_user/<int:place>',views.signin_user, name="signin"),
     path('signup_user',views.signup_user, name="signup"),
     path('logout_user',views.logout_user, name="logout")
 ]
