29a1a96c1405f6a7a23216bcb70cc669feb8e4ac
ThongP <gbrider1403@gmail.com>
2022-09-08 11:03:29 +0700

    Fix OrderItem Model

--- src/shoppingcart/models.py
+++ src/shoppingcart/models.py
@@ -66,8 +66,8 @@ class Order(models.Model):
         return shipping
 
 class OrderItem(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
-    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, blank=True, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
